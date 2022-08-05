3a425768107a567653ee6cc94759e0067d38915f
Ramiro <ramifazio@gmail.com>
2022-08-05 11:12:33 -0300

    Like Button ready

--- main.js
+++ main.js
@@ -10,11 +10,30 @@ const swiper = new Swiper(".mySwiper", {
 
 function like() {
     const likeBtnPath = document.getElementById("likeBtnPath");
+    const likeBtnSvg = document.getElementById("likeBtnSvg");
     if (likeBtnPath.style.fill !== "white") {
         likeBtnPath.style.fill = "white"
         likeBtnPath.style.stroke = "black"
     } else {
         likeBtnPath.style.fill = "#ED4956"
         likeBtnPath.style.stroke = "none"
+        likeBtnSvg.animate([
+            {
+                transform: "scale(1)"
+            },
+            {
+                transform: "scale(1.4)"
+            },
+            {
+                transform: "scale(1)"
+            },
+        ],
+            {
+                direction: "alternate",
+                duration: 200,
+                iterations: 1,
+            }
+
+        )
     }
 }
